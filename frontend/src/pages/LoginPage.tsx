3ceb12efc9ca266d9b893d313f61f24852aea735
Nelsonn <oonelsoncodes@gmail.com>
2025-06-25 14:10:51 +0100

    feat: did some cleanup and added routing

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+interface ILoginPageProps {}
+
+const LoginPage: React.FC<ILoginPageProps> = () => {
+  return (
+    <main>
+      <h1>logn/signup</h1>
+    </main>
+  );
+};
+
+export default LoginPage;
