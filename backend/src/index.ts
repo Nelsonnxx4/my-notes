fa1c6d4707f9661b501bb3b40f69a3e01899f7de
Nelsonn <oonelsoncodes@gmail.com>
2025-07-01 02:18:49 +0100

    setup v2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,32 @@
 import express from "express";
-// import usersRouter from "./routes/users";
+import http from "http";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import mongoose, { mongo } from "mongoose";
 
 const app = express();
 
-const PORT = 3000;
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
-// app.use("/api/users", usersRouter);
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`running on port ${PORT}`);
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log(`serving runing on http://localhost:8080`);
 });
+
+const MONGO_URL =
+  "mongodb+srv://oonelsoncodes:MJoxsxtSmlGs8LJs@notes-app.jvi1tl5.mongodb.net/?retryWrites=true&w=majority&appName=notes-app";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on(`error`, (error, Error) => console.log);
