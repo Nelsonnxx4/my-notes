d935f5580c57fd438c5aae238587ea0fc8e56bc0
Nelson <oonelsoncodes@gmail.com>
2026-06-22 15:21:51 +0100

    v1 of implementing the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Input } from "@heroui/input";
+import { Input, Breadcrumbs, Select } from "@heroui/react";
+import { SearchIcon } from "lucide-react";
 
 import { FilterIcon } from "@/assets/icons";
 
@@ -7,16 +8,24 @@ interface IHeaderProps {}
 const Header: React.FC<IHeaderProps> = () => {
   return (
     <header className="flex items-start gap-6 flex-col p-2 border-b border-gray-200 mb-4">
-      <div className="flex justify-between items-center w-full">
-        <h3 className="font-bold text-indigo-600 text-2xl">Just Notes</h3>
-        <picture className="bg-black rounded-full h-8 w-8 border-2 border-indigo-400">
-          <img alt="" src="" />
-        </picture>
-      </div>
-      <div className="flex justify-between items-center w-full">
-        <Input className="max-w-[80%] " placeholder="Search for notes" />
-        <FilterIcon className="cursor-pointer" />
-      </div>
+      <Breadcrumbs>
+        <Breadcrumbs.Item href="#">Home</Breadcrumbs.Item>
+        <Breadcrumbs.Item href="#">All Notes</Breadcrumbs.Item>
+      </Breadcrumbs>
+
+      <section>
+        <div>
+          <SearchIcon />
+          <Input placeholder="Search a note" />
+        </div>
+
+        <div>
+          <div>
+            <FilterIcon />
+            <Select />
+          </div>
+        </div>
+      </section>
     </header>
   );
 };
