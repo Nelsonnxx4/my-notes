d9e63b74dbc2f354d7f712477c161e1b78022531
Nelson <oonelsoncodes@gmail.com>
2026-06-25 16:45:03 +0100

    feat/UI: re-design done

--- frontend/src/components/notes/NoteCard.tsx
+++ frontend/src/components/notes/NoteCard.tsx
@@ -1,3 +1,5 @@
+import { MoreHorizontalIcon, PlusIcon } from "lucide-react";
+
 interface NoteCardProps {
   title: string;
   content: string;
@@ -10,18 +12,26 @@ const NoteCard: React.FC<NoteCardProps> = ({ title, content, color }) => {
       className={`min-w-60 h-90 overflow-hidden rounded-2xl border border-gray-300 p-4  transition-all hover:-translate-y-0.5 hover:shadow-lg ${color}`}
     >
       <div className="mb-4 flex items-center justify-between">
-        <span className="rounded-full bg-white/70 px-3 py-1 text-xs font-semibold uppercase tracking-[0.18em] text-slate-700">
-          Note
-        </span>
-        <div className="h-10 w-10 rounded-3xl bg-white/80 shadow-sm" />
+        <h3 className="rounded-full bg-white/70 px-3 py-1 text-xs font-semibold  text-slate-700">
+          Psychology
+        </h3>
+
+        <div className="flex justify-between items-center text-gray-700">
+          <PlusIcon className="cursor-pointer" strokeWidth={1.5} />
+          <MoreHorizontalIcon className="cursor-pointer" strokeWidth={1.5} />
+        </div>
       </div>
 
-      <h3 className="mb-3 text-lg font-semibold text-slate-900">{title}</h3>
-      <p className="line-clamp-5 text-sm leading-6 text-slate-700">{content}</p>
+      <div className="relative flex flex-col bg-white rounded-2xl p-4 h-[88%]">
+        <h3 className="mb-3 text-lg font-semibold text-slate-900">{title}</h3>
+        <p className="line-clamp-5 text-sm leading-6 text-slate-700">
+          {content}
+        </p>
 
-      <div className="mt-5 flex items-center justify-between text-xs text-slate-600">
-        <span>2h ago</span>
-        <span className="rounded-full bg-white/80 px-3 py-1">Updated</span>
+        <div className="mt-5 absolute bottom-2 flex items-baseline justify-between text-xs text-slate-600">
+          <span>2h ago</span>
+          <span className="rounded-full bg-white/80 px-3 py-1">Updated</span>
+        </div>
       </div>
     </article>
   );
